4a9bfaa2647bd360f001ad32d3e9c950a7102dd2
Ranger5120 <1269883269@qq.com>
2019-11-07 19:05:33 +0800

    更改

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -2,23 +2,30 @@ int ledDelay=3000;
 int redPin=10;
 int greenPin=8;
 int yellowPin=9;
+int greenPin2=7;
+int redPin2=4;
 
 void setup()
 {
     pinMode(redPin,OUTPUT);
+    pinMode(redPin2,OUTPUT);
     pinMode(yellowPin,OUTPUT);
     pinMode(greenPin,OUTPUT);
+    pinMode(greenPin2,OUTPUT);
 }
 
 void loop()
 {
     //state 1
     digitalWrite(redPin,HIGH);
+    digitalWrite(greenPin2,HIGH);
     delay(ledDelay);
     digitalWrite(redPin,LOW);
+    digitalWrite(greenPin2,LOW);
 
     //state 2
     digitalWrite(yellowPin,HIGH);
+    digitalWrite(redPin2,HIGH);
     delay(1000);
     digitalWrite(yellowPin,LOW);
 
@@ -31,5 +38,5 @@ void loop()
     digitalWrite(yellowPin,HIGH);
     delay(1000);
     digitalWrite(yellowPin,LOW);
+    digitalWrite(redPin2,LOW);
 }
-
